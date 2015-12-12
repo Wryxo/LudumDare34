ff30a0b15f26ac1bd28c1d629d6e5c4e8c7c760c
Martin Strapko <wryxsk@gmail.com>
2015-12-13 00:46:41 +0100

    Takmer funkcna verzia hry ? potencialne vyladit vyrabanie corov, movement a dobalancovat to...

--- ArnieVsAlien/Assets/Scripts/AlienCellScript.cs
+++ ArnieVsAlien/Assets/Scripts/AlienCellScript.cs
@@ -5,29 +5,31 @@ public class AlienCellScript : MonoBehaviour {
 
     public bool Attunement, IsCore;
     public Sprite OneSprite, ZeroSprite, CoreSprite, CellSprite;
-    public float MitosisCD, DeadCD;
-    public GameObject Cell;
 
-    private float currentCD;
-    public bool mitosisEnabled, dead;
-    public int numAdj, currAdj;
-    private GameObject[] adjacent = new GameObject[4]; // left, up, right, down
+    private int x, y;
+    private GameControllerScript gc;
 
 	// Use this for initialization
 	void Start () {
-        dead = false;
-        adjacent[0] = null;
-        adjacent[1] = null;
-        adjacent[2] = null;
-        adjacent[3] = null;
-        numAdj = 1;
-        currAdj = 0;
-        IsCore = false;
-        if (Random.value > 0.9f)
+        gc = GameObject.FindGameObjectWithTag("GameController").GetComponent <GameControllerScript>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+       
+    }
+    
+    public void SetCoord(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void SetCore(bool value)
+    {
+        IsCore = value;
+        if (!IsCore)
         {
-            IsCore = true;
-        }
-        if (!IsCore) { 
             Attunement = false;
             if (Random.value > 0.5f)
             {
@@ -36,160 +38,15 @@ public class AlienCellScript : MonoBehaviour {
             GetComponent<SpriteRenderer>().sprite = CellSprite;
             GetComponent<BoxCollider2D>().enabled = true;
             transform.Find("Attunement").GetComponent<SpriteRenderer>().sprite = Attunement ? OneSprite : ZeroSprite;
-            transform.Find("Attunement").GetComponent<SpriteRenderer>().color = new Color(255,0,0,255);
-            numAdj = 2;
-        } else
+            transform.Find("Attunement").GetComponent<SpriteRenderer>().color = new Color(255, 0, 0, 255);
+        }
+        else
         {
             GetComponent<SpriteRenderer>().sprite = CoreSprite;
             GetComponent<BoxCollider2D>().enabled = false;
             transform.Find("Attunement").GetComponent<SpriteRenderer>().sprite = null;
-            transform.Find("Attunement").GetComponent<SpriteRenderer>().color = new Color(255,255,255,0);
-            numAdj = 4;
+            transform.Find("Attunement").GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
         }
-        mitosisEnabled = true;
-        currentCD = IsCore ? 0 : MitosisCD;
-    }
-
-    // Update is called once per frame
-    void FixedUpdate () {
-        if (!dead) {
-            currAdj = 0;
-            for (int i=0; i < 4; i++)
-            {
-                if (adjacent[i] != null)
-                    currAdj++;
-            }
-	        if (currentCD <= 0)
-            {
-                if (mitosisEnabled) { 
-                    Mitosis();
-                    mitosisEnabled = false;
-                }
-            }
-            if (mitosisEnabled)
-                currentCD -= Time.deltaTime;
-            if (currAdj <= 0 && IsCore)
-            {
-                dead = true;
-                GetComponent<SpriteRenderer>().color = new Color(255, 0, 0, 255);
-                currentCD = DeadCD;
-                return;
-            }
-            if (currAdj < numAdj && !mitosisEnabled)
-            {
-                mitosisEnabled = true;
-                currentCD = MitosisCD;
-            }
-        } else {
-            currentCD -= Time.deltaTime;
-            if (currentCD <= 0)
-                Destroy(gameObject);
-        }
-    }
-
-    void SetAdjacent(int index, GameObject adj)
-    {
-        adjacent[index] = adj;
-    }
-
-    // GENERATE NEXT CELL, POTENTIAL CORE
-    void Mitosis()
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            if (currAdj >= numAdj)
-                break;
-            if (Random.value < (0.25f + currAdj*0.25f ))
-            {
-                var offset = new Vector3(0, 0, 0);
-                switch(i)
-                {
-                    case 0:
-                        offset.x = -2f;
-                        //offset.y = -0.5f + Random.value;
-                        break;
-                    case 1:
-                        offset.y = 2f;
-                        //offset.x = -0.5f + Random.value;
-                        break;
-                    case 2:
-                        offset.x = 2f;
-                        //offset.y = -0.5f + Random.value;
-                        break;
-                    case 3:
-                        offset.y = -2f;
-                        //offset.x = -0.5f + Random.value;
-                        break;
-                }
-                adjacent[i] = Instantiate(Cell, transform.position + offset, Quaternion.identity) as GameObject;
-                switch (i)
-                {
-                    case 0:
-                        adjacent[i].GetComponent<AlienCellScript>().SetAdjacent(2, gameObject);
-                        break;
-                    case 1:
-                        adjacent[i].GetComponent<AlienCellScript>().SetAdjacent(3, gameObject);
-                        break;
-                    case 2:
-                        adjacent[i].GetComponent<AlienCellScript>().SetAdjacent(0, gameObject);
-                        break;
-                    case 3:
-                        adjacent[i].GetComponent<AlienCellScript>().SetAdjacent(1, gameObject);
-                        break;
-                }
-            }
-        }
-    }
-
-    bool CheckCore(int from)
-    {
-        if (IsCore)
-            return true;
-        for (int i=0; i < 4; i++)
-        {
-            if (i == from) {
-                Debug.Log("Koniec!");
-                continue;
-            }
-            if (adjacent[i] != null)
-            {
-                switch (i) { 
-                    case 0:
-                        if (adjacent[i].GetComponent<AlienCellScript>().CheckCore(2))
-                            return true;
-                        break;
-                    case 1:
-                        if (adjacent[i].GetComponent<AlienCellScript>().CheckCore(3))
-                            return true;
-                        break;
-                    case 2:
-                        if (adjacent[i].GetComponent<AlienCellScript>().CheckCore(0))
-                            return true;
-                        break;
-                    case 3:
-                        if (adjacent[i].GetComponent<AlienCellScript>().CheckCore(1))
-                            return true;
-                        break;
-                }
-            }
-        }
-        if (Random.value < 0.25f)
-        {
-            Mutate();
-            return true;
-        }
-        return false;
-    }
-
-    //CELL BECOMES CORE
-    void Mutate()
-    {
-        IsCore = true;
-        GetComponent<SpriteRenderer>().sprite = CoreSprite;
-        transform.Find("Attunement").GetComponent<SpriteRenderer>().sprite = null;
-        GetComponent<BoxCollider2D>().enabled = false;
-        mitosisEnabled = true;
-        currentCD = 0;
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -197,31 +54,7 @@ public class AlienCellScript : MonoBehaviour {
         if (other.tag == "Projectile")
         {
             if (other.GetComponent<ProjectileScript>().Attunement != Attunement) {
-                for (int i = 0; i < 4; i++)
-                {
-                    if (adjacent[i] != null)
-                    {
-                        switch (i)
-                        {
-                            case 0:
-                                adjacent[i].GetComponent<AlienCellScript>().CheckCore(2);
-                                adjacent[i].GetComponent<AlienCellScript>().SetAdjacent(2, null);
-                                break;
-                            case 1:
-                                adjacent[i].GetComponent<AlienCellScript>().CheckCore(3);
-                                adjacent[i].GetComponent<AlienCellScript>().SetAdjacent(3, null);
-                                break;
-                            case 2:
-                                adjacent[i].GetComponent<AlienCellScript>().CheckCore(0);
-                                adjacent[i].GetComponent<AlienCellScript>().SetAdjacent(0, null);
-                                break;
-                            case 3:
-                                adjacent[i].GetComponent<AlienCellScript>().CheckCore(1);
-                                adjacent[i].GetComponent<AlienCellScript>().SetAdjacent(1, null);
-                                break;
-                        }
-                    }
-                }
+                gc.DestroyAlien(x, y);
                 Destroy(gameObject);
             }
         }
---
+++ ArnieVsAlien/Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,264 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameControllerScript : MonoBehaviour
+{
+
+    public GameObject Cell;
+    public float MitosisCD;
+    public float currentCD;
+
+    public int numCores;
+    private int vertExtent, horzExtent;
+    private AlienCellScript[][] map;
+    private short[][] bfsMap;
+    private int maxCore, maxCell;
+
+    // Use this for initialization
+    void Start()
+    {
+        vertExtent = (int)Camera.main.orthographicSize;
+        horzExtent = vertExtent * Screen.width / Screen.height;
+        map = new AlienCellScript[horzExtent][];
+        for (int i = 0; i < horzExtent; i++)
+        {
+            map[i] = new AlienCellScript[vertExtent];
+            for (int j = 0; j < vertExtent; j++)
+            {
+                map[i][j] = null;
+            }
+        }
+        maxCore = 3;
+        maxCell = 1;
+        for (int i = 0; i < 3; i++)
+        {
+            int x = Random.Range(0, horzExtent);
+            int y = Random.Range(0, vertExtent);
+            SpawnAlien(x, y);
+            map[x][y].SetCore(true);
+            Mitosis(x, y);
+        }
+        currentCD = MitosisCD / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentCD -= Time.deltaTime;
+        if (currentCD <= 0)
+        {
+            numCores = 0;
+            for (int i = 0; i < horzExtent; i++)
+            {
+                for (int j = 0; j < vertExtent; j++)
+                {
+                    if (map[i][j] != null)
+                    {
+                        if (!map[i][j].IsCore)
+                            Mitosis(i, j);
+                        else
+                        {
+                            if (CheckAlive(i,j))
+                            {
+                                Mitosis(i, j);
+                                numCores++;
+                            }
+                        }
+                    }
+                }
+            }
+            if (numCores == 0) Victory();
+            currentCD = MitosisCD;
+        }
+    }
+
+    void Victory()
+    {
+        Debug.Log("VICTORY!");
+    }
+
+    bool SpawnAlien(int x, int y)
+    {
+        if (x >= 0 && x < horzExtent && y >= 0 && y < vertExtent && map[x][y] == null)
+        {
+            var go = Instantiate(Cell, new Vector3(-19 + x * 2, -14 + y * 2, 0), Quaternion.identity) as GameObject;
+            map[x][y] = go.GetComponent<AlienCellScript>();
+            map[x][y].SetCoord(x, y);
+            if (Random.value > 0.9)
+            {
+                map[x][y].SetCore(true);
+            }
+            else
+            {
+                map[x][y].SetCore(false);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    void Mitosis(int x, int y)
+    {
+        int numAdj = 0;
+        for (int i = x-1; i < x+2; i++)
+        {
+            for (int j = y-1; j < y+2; j++)
+            {
+                if (i == x && j == y) continue;
+                if (i < 0 || i >= horzExtent || j < 0 || j >= vertExtent) continue;
+                if (map[i][j] != null) numAdj++;
+            }
+        }
+        if (map[x][y].IsCore)
+        {
+            for (int i = 0; i < maxCore; i++)
+            {
+                int offx = Random.Range(-1, 2);
+                int offy = Random.Range(-1, 2);
+                if (x+offx < 0 || x+offx >= horzExtent || y+offy < 0 || y+offy >= vertExtent) continue;
+                if (map[x+offx][y+offy] == null)
+                {
+                    if (SpawnAlien(x + offx, y + offy))
+                    {
+                        numAdj++;
+                    }
+                }
+            }
+
+        } else
+        {
+            for (int i = 0; i < maxCell; i++)
+            {
+                int offx = Random.Range(-1, 2);
+                int offy = Random.Range(-1, 2);
+                if (x+offx < 0 || x+offx >= horzExtent || y+offy < 0 || y+offy >= vertExtent) continue;
+                if (map[x + offx][y + offy] == null)
+                {
+                    if (SpawnAlien(x+offx, y+offy))
+                    {
+                        numAdj++;
+                    }
+                }
+            }
+            /*for (int i = x - 1; i < x + 2; i++)
+            {
+                for (int j = y - 1; j < y + 2; j++)
+                {
+                    if (i == x && j == y) continue;
+                    if (i < 0 || i >= horzExtent || j < 0 || j >= vertExtent) continue;
+                    if (SpawnAlien(i, j))
+                    {
+                        numAdj++;
+                    }
+                    if (numAdj >= maxCell) break;
+                }
+                if (numAdj >= maxCell) break;
+            }*/
+        }
+    }
+
+    public void DestroyAlien(int x, int y)
+    {
+        bfsMap = new short[horzExtent][];
+        for (int i = 0; i < horzExtent; i++)
+        {
+            bfsMap[i] = new short[vertExtent];
+            for (int j = 0; j < vertExtent; j++)
+            {
+                bfsMap[i][j] = 0;
+            }
+        }
+        /*if (map[x + 1][y] != null)
+        {
+            if (!CheckCore(x + 1, y))
+            {
+                map[x + 1][y].SetCore(true);
+            }
+        }*/
+        for (int i = x - 1; i < x + 2; i++)
+        {
+            for (int j = y - 1; j < y + 2; j++)
+            {
+                if (i == x && j == y) continue;
+                if (i < 0 || i >= horzExtent || j < 0 || j >= vertExtent) continue;
+                if (map[i][j] != null)
+                {
+                    if (!CheckCore(i, j))
+                    {
+                        map[i][j].SetCore(true);
+                    }
+                }
+            }
+        }
+    }
+
+    bool CheckAlive(int x, int y)
+    {
+        var alive = false;
+        for (int i = x - 1; i < x + 2; i++)
+        {
+            for (int j = y - 1; j < y + 2; j++)
+            {
+                if (i == x && j == y) continue;
+                if (i < 0 || i >= horzExtent || j < 0 || j >= vertExtent) continue;
+                if (map[i][j] != null && !map[i][j].IsCore)
+                {
+                    alive = true;
+                }
+            }
+        }
+        if (!alive) { 
+            Destroy(map[x][y].gameObject);
+            return false;
+        }
+        return true;
+    }
+
+    bool CheckCore(int x, int y)
+    {
+        if (bfsMap[x][y] != 0)
+            return bfsMap[x][y] > 0;
+        bfsMap[x][y] = -1;
+        if (map[x][y].IsCore)
+        {
+            bfsMap[x][y] = 1;
+            return true;
+        }
+        for (int i = x - 1; i < x + 2; i++)
+        {
+            for (int j = y - 1; j < y + 2; j++)
+            {
+                if (i == x && j == y) continue;
+                if (i < 0 || i >= horzExtent || j < 0 || j >= vertExtent) continue;
+                if (map[i][j] != null && bfsMap[i][j] == 0)
+                {
+                    if (CheckCore(i, j))
+                    {
+                        bfsMap[x][y] = 1;
+                        return true;
+                    }
+                }
+            }
+        }
+        //if (map[x + 1][y] != null) if (CheckCore(x + 1, y)) return true;
+        return false;
+    }
+}
+
+public class Pair
+{
+    int x;
+    int y;
+
+    public Pair (int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public override string ToString()
+    {
+        return x + " " + y;
+    }
+}
